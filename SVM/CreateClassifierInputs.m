1e7c885f56bb5f4fadd339d1318de65c99dc6c89
Joy Patel <joypatel100@yahoo.com>
2014-06-17 13:58:32 -0400

    SVM
    More convenient way to use SVM.  Still have to write function which
    allows you to add to an already existing svm.

--- SVM/CreateClassifierInputs.m
+++ SVM/CreateClassifierInputs.m
@@ -1,26 +1,36 @@
-function [ X,Y ] = CreateClassifierInputs( pass, passNum, fail, failNum )
+function [ X,Y ] = CreateClassifierInputs( passDir, failDir)
 %creates input for SVM classifier
 %pass and fail are the filenames for the passing and failing cases
-%all the files in pass and fail must be names train#.png
+%all the files in pass and fail must be named train#.png
+%make sure both folders are added to path
+%make sure you are not in the directories for your current folder
 
-X=zeros(passNum+failNum,12);
-Y=zeros(passNum+failNum,1);
+fileP=dir(passDir);
+[sizeP,temp1]=size(fileP);
+
+fileF=dir(failDir);
+[sizeF,temp2]=size(fileF);
+
+X=[];
+Y=[];
 
 %making X
-for i=1:1:passNum+failNum;
-    if i<=passNum
-        prop=RegionPropVector(strcat(pass,'/train',int2str(i),'.png'));
-    else
-        prop=RegionPropVector(strcat(fail,'/train',int2str(i-passNum),'.png'));
-    end;
-    for j=1:1:12;
-        X(i,j)=prop(j,1);
+for i=1:1:sizeP;
+    data=fileP(i);
+    if isequal(strfind(data.name,'train'),[])==0 %name contains 'train'
+        prop=RegionPropVector(strcat(passDir,'/',data.name));
+        X=[X;prop];
+        Y=[Y;[1]];
     end;
 end;
 
-%making Y
-for k=1:1:passNum;
-    Y(k,1)=1;
+for j=1:1:sizeF;
+    data=fileF(j);
+    if isequal(strfind(data.name,'train'),[])==0 %name contains 'train'
+        prop=RegionPropVector(strcat(failDir,'/',data.name));
+        X=[X;prop];
+        Y=[Y;[0]];
+    end;
 end;
 
 end
